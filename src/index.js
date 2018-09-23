d84f82e7be522f4968c60da85cdac117cf78d399
tatyana-andreichyk <tatyana.andreichyk@gmail.com>
2018-09-23 23:39:21 +0300

    sorter/feature: implement class Sorter with methods

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
-class Sorter {
+class Sorter {  
+    
   constructor() {
-    // your implementation
+    this.array = [];
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    let sortedArray = [];
+    indices = indices.sort(function compareFunction(a, b) {
+        return a - b;
+    });
+    for (let i = 0; i < indices.length; i++) {
+       sortedArray.push(this.array[indices[i]]); 
+    }
+  
+    sortedArray.sort(this.comparator);
+    
+    for (let i = 0; i < indices.length; i++) {
+       this.array[indices[i]] = sortedArray.shift(); 
+    }
+    
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    
+    this.comparator = compareFunction;
+    
   }
+  
 }
 
 module.exports = Sorter;
